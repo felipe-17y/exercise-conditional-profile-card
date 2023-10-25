cac3b3a0ca87208b1829f5da7d52d3886e13087e
felipe-17y <edwardfel@hotmail.com>
2023-10-25 23:56:47 +0000

    card

--- src/js/app.js
+++ src/js/app.js
@@ -1,36 +1,10 @@
 import "../style/index.css";
-
-/**
- *  EDIT ONLY INSIDE THIS RENDER FUNCTION
- *  This function is called every time the user changes types or changes any input
- * 
-    {
-        includeCover: true, // if includeCover is true the algorithm should
-        background: "https://images.unsplash.com/photo-1511974035430-5de47d3b95da", // this is the url of the image that will used as background for the profile cover
-        avatarURL: "https://randomuser.me/api/portraits/women/42.jpg", // this is the url for the profile avatar
-        socialMediaPosition: "left", // social media bar position (left or right)
-        
-        twitter: null, // social media usernames
-        github: null,
-        linkedin: null,
-        instagram: null,
-
-        name: null,
-        lastname: null,
-        role: null,
-        country: null,
-        city: null
-    }
- */
-
 function render(variables = {}) {
-  console.log("These are the current variables: ", variables); //print on the console
-  // here we ask the logical questions to make decisions on how to build the html
-  // if includeCover==false then we reset the cover code without the <img> tag to make the cover transparent.
+  console.log("These are the current variables: ", variables);
   let cover = `<div class="cover"><img src="${variables.background}" /></div>`;
   if (variables.includeCover == false) cover = "<div class='cover'></div>";
-  if (variables.name == null) variables.name = "Name";
-  if (variables.lastname == null) variables.lastname = "tebar";
+  if (variables.name == null) variables.name = "name";
+  if (variables.lastname == null) variables.lastname = "last name";
   if (variables.twitter == null) variables.twitter = "";
   if (variables.github == null) variables.github = "";
   if (variables.linkedin == null) {
@@ -60,9 +34,6 @@ function render(variables = {}) {
     `;
 }
 
-/**
- * Don't change any of the lines below, here is where we do the logic for the dropdowns
- */
 window.onload = function() {
   window.variables = {
     // if includeCover is true the algorithm should
@@ -77,7 +48,7 @@ window.onload = function() {
     socialMediaPosition: "position-left",
     // social media usernames
     twitter: null,
-    github: "alesanchezr",
+    github: "felipe-17y",
     linkedin: null,
     instagram: null,
     name: null,
@@ -86,7 +57,7 @@ window.onload = function() {
     country: null,
     city: null
   };
-  render(window.variables); //render the card for the first time
+  render(window.variables);
 
   document.querySelectorAll(".picker").forEach(function(elm) {
     elm.addEventListener("change", function(e) {
